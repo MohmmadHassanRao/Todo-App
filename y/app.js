eaaaa358bb6d82fd4b34a6ae7c0cfdfdfffc9f39
MohammadHassanRao <hassanribery0335@gmail.com>
2020-08-20 17:22:18 +0500

    connected with firebase database

--- y/app.js
+++ y/app.js
@@ -1,38 +1,53 @@
 var list = document.getElementById("list");
+firebase
+  .database()
+  .ref("todos")
+  .on("child_added", function (data) {
+    var li = document.createElement("li");
+    var liText = document.createTextNode(data.val().value);
+    li.appendChild(liText);
 
+    //  Delete Button
+
+    var delBtn = document.createElement("button");
+    var delImg = document.createElement("img");
+    delImg.src = "./images/bin.png";
+    delImg.style.height = "35px";
+    delBtn.setAttribute("id", data.val().key);
+    delBtn.appendChild(delImg);
+    delBtn.setAttribute("onclick", "deleteTodo(this)");
+
+    // Edit Button
+
+    var editBtn = document.createElement("button");
+    var editImg = document.createElement("img");
+    editImg.src = "./images/edit.png";
+    editImg.style.height = "35px";
+    editBtn.setAttribute("id", data.val().key);
+    editBtn.appendChild(editImg);
+    editBtn.setAttribute("onclick", "editItem(this)");
+
+    li.appendChild(editBtn);
+    li.appendChild(delBtn);
+    list.appendChild(li);
+  });
 function addTodo() {
   // create lists
   var todoItem = document.getElementById("todo-item");
-  var li = document.createElement("li");
-  var liText = document.createTextNode(todoItem.value);
-  li.appendChild(liText);
-
-  //  Delete Button
-
-  var delBtn = document.createElement("button");
-  var delImg = document.createElement("img");
-  delImg.src = "./images/bin.png";
-  delImg.style.height = "35px";
-  delBtn.appendChild(delImg);
-  delBtn.setAttribute("onclick", "deleteTodo(this)");
-
-  // Edit Button
-
-  var editBtn = document.createElement("button");
-  var editImg = document.createElement("img");
-  editImg.src = "./images/edit.png";
-  editImg.style.height = "35px";
-  editBtn.appendChild(editImg);
-  editBtn.setAttribute("onclick", "editItem(this)");
-
-  li.appendChild(editBtn);
-  li.appendChild(delBtn);
-  list.appendChild(li);
+  var firebase_database = firebase.database().ref("todos");
+  var key = firebase_database.push().key;
+  var fire_todo = {
+    value: todoItem.value,
+    key: key,
+  };
+  firebase_database.child(key).set(fire_todo);
 
   todoItem.value = "";
 }
 //Delete Todo
 function deleteTodo(e) {
+  //Removing from database
+  firebase.database().ref("todos").child(e.id).remove();
   var animateList = e.parentNode;
   animateList.classList.add("animate__animated", "animate__backOutRight");
 }
@@ -42,9 +57,16 @@ function editItem(e) {
   var defaultTodo = e.parentNode.textContent;
   var editTodo = prompt("Edit your Todo Item", defaultTodo);
   e.parentNode.firstChild.nodeValue = editTodo;
+  var edit_firebaseTodo = {
+    value: editTodo,
+    key: e.id,
+  };
+  firebase.database().ref("todos").child(e.id).set(edit_firebaseTodo);
+  console.log(edit_firebaseTodo);
 }
 
 //Delete All todos
 function deleteAll() {
+  firebase.database().ref("todos").remove();
   list.innerHTML = "";
 }
